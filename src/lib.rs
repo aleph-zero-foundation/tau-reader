e09fd2350f3dd10b6161ddeef4fb483640e7658d
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-11-30 13:57:47 +0100

    G2 points supported as well

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,35 @@ pub mod aws_access;
 mod offsets;
 mod parameters;
 
-use crate::parameters::{BUCKET_NAME, G1_SIZE, NUMBER_OF_POWERS};
+use crate::parameters::{BUCKET_NAME, G1_SIZE, G2_SIZE, NUMBER_OF_POWERS};
 use aws_sdk_s3::Client;
-use bellman::pairing::bn256::Bn256;
-use bellman::pairing::{bn256::G1Affine, CurveAffine, EncodedPoint, Engine};
+use bellman::pairing::{
+    bn256::{Bn256, G1Affine, G2Affine},
+    CurveAffine, EncodedPoint, Engine,
+};
 use std::io::Read;
 
 type Chunk = &'static str;
+type G1Encoded = <<Bn256 as Engine>::G1Affine as CurveAffine>::Compressed;
+type G2Encoded = <<Bn256 as Engine>::G2Affine as CurveAffine>::Compressed;
 
 pub async fn get_g1(index: usize, client: &Client) -> G1Affine {
     assert!(index < NUMBER_OF_POWERS, "index out of bounds");
     let (chunk, inner_offset) = offsets::compute_g1_position(index);
+    read_point::<G1_SIZE, G1Encoded>(chunk, inner_offset, client).await
+}
 
+pub async fn get_g2(index: usize, client: &Client) -> G2Affine {
+    assert!(index < NUMBER_OF_POWERS, "index out of bounds");
+    let (chunk, inner_offset) = offsets::compute_g2_position(index);
+    read_point::<G2_SIZE, G2Encoded>(chunk, inner_offset, client).await
+}
+
+async fn read_point<const POINT_SIZE: usize, Enc: EncodedPoint>(
+    chunk: Chunk,
+    inner_offset: usize,
+    client: &Client,
+) -> Enc::Affine {
     let mut bytes = Vec::new();
 
     let mut stream = client
@@ -22,19 +39,19 @@ pub async fn get_g1(index: usize, client: &Client) -> G1Affine {
         .key(chunk)
         .range(format!(
             "bytes={inner_offset}-{}",
-            inner_offset + G1_SIZE - 1
+            inner_offset + POINT_SIZE - 1
         ))
         .send()
         .await
         .expect("must get object");
+
     while let Some(data) = stream.body.next().await {
         bytes.extend(data.expect("must read data"));
     }
 
-    assert_eq!(bytes.len(), G1_SIZE, "must read exactly G1_SIZE bytes");
+    assert_eq!(bytes.len(), POINT_SIZE);
 
-    let mut encoded =
-        <<<Bn256 as Engine>::G1Affine as CurveAffine>::Compressed as EncodedPoint>::empty();
+    let mut encoded = Enc::empty();
     bytes.as_slice().read_exact(encoded.as_mut()).unwrap();
     encoded.into_affine().unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bellman::pairing::bn256::Bn256;
 use bellman::pairing::ff::{Field, ScalarEngine};
 use bellman::pairing::{CurveAffine, CurveProjective, Engine};
 
-use tau_reader::{aws_access, get_g1};
+use tau_reader::{aws_access, get_g1, get_g2};
 
 #[tokio::main]
 async fn main() -> Result<(), s3::Error> {
@@ -15,11 +15,15 @@ async fn main() -> Result<(), s3::Error> {
     let client = aws_access::access_aws().await;
 
     for i in [0, 1, 2, 3, 4, 5, 123, 10000, 1239123usize] {
-        print!("Getting G1 point for the power: `{i}`...");
+        print!("Getting points for the power: `{i}`...");
 
         let g1 = get_g1(i, &client).await;
         let expected_g1 = <Bn256 as Engine>::G1Affine::one().mul(tau.pow(&[i as u64]));
         assert_eq!(g1, expected_g1.into_affine(), "G1 {i} power is incorrect");
+
+        let g2 = get_g2(i, &client).await;
+        let expected_g2 = <Bn256 as Engine>::G2Affine::one().mul(tau.pow(&[i as u64]));
+        assert_eq!(g2, expected_g2.into_affine(), "G2 {i} power is incorrect");
 
         println!(" ✅")
     }
